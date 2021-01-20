6defc75766afaf6e0904e7cfd20a13ecb1bb4f41
Dinislam Sadigov <51309582+azerzet@users.noreply.github.com>
2021-01-20 22:42:03 +0500

    Add files via upload
    GA

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+class Music  {
+    let notes: [String]
+ 
+    init(notes: [String])  {
+        self.notes = notes
+    }
+
+    func prepared() -> String  {
+        return notes.joined(separator: " ")
+    }
+}
+// 1
+class Instrument {
+    // 2
+    let brand: String
+    // 3
+    init(brand: String) {
+        //4
+        self.brand = brand
+    }
+    
+    func tune() -> String {
+        fatalError("Implement this method for \(brand)")
+     }
+    
+}
