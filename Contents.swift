27e0ab585f9b211b7ef3ecf4b618b935cf6ca323
Dinislam Sadigov <51309582+azerzet@users.noreply.github.com>
2021-01-21 19:16:04 +0500

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -1,26 +1,2 @@
-class Music  {
-    let notes: [String]
- 
-    init(notes: [String])  {
-        self.notes = notes
-    }
-
-    func prepared() -> String  {
-        return notes.joined(separator: " ")
-    }
-}
-// 1
-class Instrument {
-    // 2
-    let brand: String
-    // 3
-    init(brand: String) {
-        //4
-        self.brand = brand
-    }
-    
-    func tune() -> String {
-        fatalError("Implement this method for \(brand)")
-     }
-    
-}
+let nameProjects = 14
+print(nameProjects)
